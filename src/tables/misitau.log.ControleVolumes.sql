77ed2472539210d44238623337f9d3693f424a3c
LeoMTal <leonard_talarico@hotmail.com>
2026-07-22 12:04:26 -0300

    Envio do script da criação da tabela de Controle de volumes

---
+++ src/tables/misitau.log.ControleVolumes.sql
@@ -0,0 +1,11 @@
+Create table misitau.log.ControleVolumes (
+	IdControleVolume int identity(1,1) constraint PKControleVolumes primary key,
+	IdExecucao int,
+	NomeTabelaOrigem varchar(128),
+	NomeTabelaDestino varchar(128),
+	LinhasOrigem int,
+	LinhasInseridas int,
+	LinhasAtualizadas int,
+	LinhasTotaisDestino int,
+	DataExecucao datetime
+)
