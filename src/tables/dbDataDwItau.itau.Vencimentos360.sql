ba4ad368a85222caee49a77cf3160849945ae0f3
JhGrillo <jh.grill2001@gmail.com>
2026-08-17 16:38:12 -0300

    Refatoramento do processo de vencimentos 360

--- src/tables/dbDataDwItau.itau.Vencimentos360.sql
+++ src/tables/dbDataDwItau.itau.Vencimentos360.sql
@@ -1,13 +1,18 @@
 Create table dbDataDwItau.itau.Vencimentos360 (
-	IdVencimento int identity constraint PkVencimentos primary key clustered,
+	IdVencimento int identity(1,1) constraint PKVencimentos360 primary key,
 	IdBase int,
 	Data datetime,
+	IdAcordo int,
+	TipoAcordo varchar(32),
 	IdDevedor int,
 	IdTitulo int,
-	IdAcordo int,
+	Plano int,
 	NumeroParcela int,
 	Valor money,
-	Referencia varchar(32),
+	Referencia varchar(64),
+	Proposta char(1),
+	DataAprovacaoProposta datetime,
+	StatusAcordo varchar(64),
 	DataCancelamento datetime,
-	IdOrigemAcordo char(1)
+	IdOrigemAcordo char(2)
 );
